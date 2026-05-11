f0475d9e9e6270e483a1d13b924c08012a869bb9
Mohit Tiwari <mohittiwari17091709@gmail.com>
2026-05-11 12:55:35 +0530

    Change router from createBrowserRouter to createHashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Home from './component/Home/Home'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import Fruits from './component/Fruits/Fruits'
 import Dairy from './component/Dairy/Dairy'
 import SeaFood from './component/SeaFood/SeaFood'
 import AllProducts from './component/AllProducts/AllProducts'
 import Layout from './component/Layout/Layout'
 const App = () => {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: '/',
       element: <Layout />,
